dda37bfca26910238147fd373669c3c8a5949652
lferrara <leo.ferrara@davikingcode.com>
2023-10-17 18:23:38 +0200

    FEAT coop 4 player

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -24,18 +24,29 @@ namespace BossTimer
     {
         public static RoutedCommand CaptureTimerCmd = new RoutedCommand();
 
-
         List<TimeSpan[]> timeList = new List<TimeSpan[]>();
         List<string> visualList = new List<string>();
+        TextBlock[] playerDeathDisplayTxtBl;
+        Label[] playerDeathDisplayLbl;
+        TimeSpan tryTime;
+        int nbPlayer = 1;
+        int[] deathPlayer = {0, 0, 0, 0};
         bool first = true;
 
         public MainWindow()
         {
             InitializeComponent();
 
+            HotkeyManager.Current.AddOrReplace("AddDeathP1", new KeyGesture(Key.NumPad1, ModifierKeys.Alt), AddDeathP1);
+            HotkeyManager.Current.AddOrReplace("AddDeathP2", new KeyGesture(Key.NumPad2, ModifierKeys.Alt), AddDeathP2);
+            HotkeyManager.Current.AddOrReplace("AddDeathP3", new KeyGesture(Key.NumPad3, ModifierKeys.Alt), AddDeathP3);
+            HotkeyManager.Current.AddOrReplace("AddDeathP4", new KeyGesture(Key.NumPad4, ModifierKeys.Alt), AddDeathP4);
             HotkeyManager.Current.AddOrReplace("CaptureTimer", new KeyGesture(Key.NumPad5, ModifierKeys.Alt), CaptureTimer);
             
             timeListbx.ItemsSource = visualList;
+            playerDeathDisplayTxtBl = new TextBlock[4] { tryTxtBl1, tryTxtBl2, tryTxtBl3, tryTxtBl4 };
+            playerDeathDisplayLbl = new Label[4] { p1Lbl, p2Lbl, p3Lbl, p4Lbl };
+            showPlayerDeathTxt();
         }
 
         private void CaptureTimer(object sender, HotkeyEventArgs e)
@@ -60,7 +71,7 @@ namespace BossTimer
             }
             else
             {
-                TimeSpan tryTime = (time - timeList.Last()[0]) * speedSlider.Value;
+                tryTime = (time - timeList.Last()[0]) * speedSlider.Value;
                 string tryMin = tryTime.Minutes < 10 ? $"0{tryTime.Minutes}" : $"{tryTime.Minutes}";
                 string trysec = tryTime.Seconds < 10 ? $"0{tryTime.Seconds}" : $"{tryTime.Seconds}";
 
@@ -73,6 +84,27 @@ namespace BossTimer
             }
         }
 
+        private void AddDeathP1(object sender, HotkeyEventArgs e)
+        {
+            deathPlayer[0]++;
+            tryTxtBl1.Text = deathPlayer[0].ToString();
+        }
+        private void AddDeathP2(object sender, HotkeyEventArgs e)
+        {
+            deathPlayer[1]++;
+            tryTxtBl2.Text = deathPlayer[1].ToString();
+        }
+        private void AddDeathP3(object sender, HotkeyEventArgs e)
+        {
+            deathPlayer[2]++;
+            tryTxtBl3.Text = deathPlayer[2].ToString();
+        }
+        private void AddDeathP4(object sender, HotkeyEventArgs e)
+        {
+            deathPlayer[3]++;
+            tryTxtBl4.Text = deathPlayer[3].ToString();
+        }
+
         private void BossBtn_Click(object sender, RoutedEventArgs e)
         {
             TimeSpan totalTime = TimeSpan.Zero;
@@ -83,10 +115,48 @@ namespace BossTimer
             }
 
             TimeTxtBl.Text = $"{totalTime.Hours}h {totalTime.Minutes}min {totalTime.Seconds}";
-            TryTxtBl.Text = (timeList.Count - 1).ToString();
+            TimeDownTxtBl.Text = $"{totalTime.Minutes}min {totalTime.Seconds}";
+            tryTotalTxtBl.Text = (timeList.Count - 1).ToString();
 
+            //values reset
+            for (int i = 0; i < 4; i++)
+            {
+                deathPlayer[i] = 0;
+            }
+
+            tryTxtBl1.Text = deathPlayer[0].ToString();
+            tryTxtBl2.Text = deathPlayer[1].ToString();
+            tryTxtBl3.Text = deathPlayer[2].ToString();
+            tryTxtBl4.Text = deathPlayer[3].ToString();
             timeList.Clear();
             visualList.Clear();
+        }
+
+        private void showPlayerDeathTxt()
+        {
+            for (int i = 0; i < 4; i++)
+            {
+                if (i < nbPlayer)
+                {
+                    playerDeathDisplayTxtBl[i].Visibility = Visibility.Visible;
+                    playerDeathDisplayLbl[i].Visibility = Visibility.Visible;
+                }
+                else
+                {
+                    playerDeathDisplayTxtBl[i].Visibility = Visibility.Hidden;
+                    playerDeathDisplayLbl[i].Visibility = Visibility.Hidden;
+                }
+            }
+        }
+
+        private void nbPlayerSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            nbPlayer = (int)nbPlayerSlider.Value;
+
+            if (IsLoaded)
+            {
+                showPlayerDeathTxt();
+            }
         }
     }
 }
